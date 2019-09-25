9994208d4c390d4cb699ac509bf962fecefeb309
Temoc Palma <cuauhtemoc@makingdevs.com>
2019-09-25 15:26:10 -0500

    Cambiar config del server ldap

--- elixir/my-phoenix-json-api/config/config.exs
+++ elixir/my-phoenix-json-api/config/config.exs
@@ -26,13 +26,13 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 config :exldap, :settings,
-  server: "ldap.forumsys.com",
-  base: "dc=example,dc=com",
+  server: "www.zflexldap.com",
+  base: "dc=zflexsoftware,dc=com",
   port: 389,
-  ssl: true,
-  user_dn: "cn=read-only-admin,dc=example,dc=com",
-  password: "password",
-  search_timeout: 1000
+  # ssl: false,
+  user_dn: "cn=ro_admin,ou=sysadmins,dc=zflexsoftware,dc=com",
+  password: "zflexpass",
+  search_timeout: 5000
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
