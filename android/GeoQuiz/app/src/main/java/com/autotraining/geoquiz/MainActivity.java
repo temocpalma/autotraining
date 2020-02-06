361b7bedb4dfae97554d84520454f1dcd3591c2b
Temoc Palma <cuauhtemoc@makingdevs.com>
2020-02-06 12:15:28 -0600

    Cap 1, challenge 1, change toast position

--- android/GeoQuiz/app/src/main/java/com/autotraining/geoquiz/MainActivity.java
+++ android/GeoQuiz/app/src/main/java/com/autotraining/geoquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.autotraining.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 200);
+                toast.show();
             }
         });
 
@@ -29,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,200);
+                toast.show();
             }
         });
 
